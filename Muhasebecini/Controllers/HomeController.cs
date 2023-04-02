ca65a66601fd05bfa026b1fd9df7edccc3d70e52
tufan35 <34610153+tufan35@users.noreply.github.com>
2023-04-02 23:26:14 +0300

    Temalar yenilenmiştir

--- Muhasebecini/Controllers/HomeController.cs
+++ Muhasebecini/Controllers/HomeController.cs
@@ -1,36 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Muhasebecini.Entities;
-using Muhasebecini.Models;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Muhasebecini.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private UnitOfWork.UnitOfWork _unitOfWork;
-        private DatabaseContext _context;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-            _unitOfWork = new UnitOfWork.UnitOfWork(_context);
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
